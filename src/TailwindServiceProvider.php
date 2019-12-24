1dff82d3060dbd5d75e73fd3c6263e192d7d13cf
warmiak <peter.wolkiewicz@orendo.de>
2019-12-24 20:00:14 +0100

    Initial Commit

---
+++ src/TailwindServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Warmiak\LaravelUi;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Ui\UiCommand;
+
+class TailwindServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        UiCommand::macro('tailwind', function ($command) {
+            $command->info('its working !.');
+        });
+    }
+}
